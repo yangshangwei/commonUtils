ca6f34518a40519ca1dbe49eda837e124eab2f06
Mr.Yang <815150141@qq.com>
2017-08-28 14:33:52 -0400

    获取项目文件路径调整

--- src/main/java/com/artisan/commonUtils/getPath/PathUtil.java
+++ src/main/java/com/artisan/commonUtils/getPath/PathUtil.java
@@ -19,7 +19,6 @@ public class PathUtil {
 	 */
 	public static String getClassPath(){
 		String systemName = System.getProperty("os.name");
-		System.out.println("systemName" + systemName);
 		//判断当前环境，如果是Windows 要截取路径的第一个 '/'
 		if(!StringUtils.isBlank(systemName) && systemName.indexOf("Windows") !=-1){
 			return PathUtil.class.getResource("/").getFile().toString().substring(1);
@@ -27,6 +26,7 @@ public class PathUtil {
 			return PathUtil.class.getResource("/").getFile().toString();
 		}
 	}
+	
 	/**
 	 * 获取当前对象的路径
 	 * @param object
@@ -35,35 +35,16 @@ public class PathUtil {
 	public static String getObjectPath(Object object){
 		return object.getClass().getResource(".").getFile().toString();
 	}
+	
 	/**
 	 * 获取到项目的路径
 	 * @return path
 	 */
 	public static String getProjectPath(){
-		System.out.println(System.getProperty("user.dir"));
 		return System.getProperty("user.dir");
 	}
-	/**
-	 * 获取 root目录
-	 * @return path
-	 */
-	public static String getRootPath(){
-		return getWEB_INF().replace("WEB-INF/", "");
-	}
-	/**
-	 * 获取输出HTML目录
-	 * @return
-	 */
-	public static String getHTMLPath(){
-		return getFreePath() + "html/html/";
-	}
-	/**
-	 * 获取输出FTL目录
-	 * @return
-	 */
-	public static String getFTLPath(){
-		return getFreePath() + "html/ftl/";
-	}
+	
+	
 	/**
 	 * 获取 web-inf目录
 	 * @return path
@@ -71,35 +52,15 @@ public class PathUtil {
 	public static String getWEB_INF(){
 		return getClassPath().replace("classes/", "");
 	}
+	
 	/**
-	 * 获取模版文件夹路径
-	 * @return path
-	 */
-	public static String getFreePath(){
-		return getWEB_INF() + "ftl/";
-	}
-	/**
-	 * 文本换行，因为Linux  和 Windows 的换行符不一样
+	 * 文本换行符号
 	 * @return
 	 */
 	public static String nextLine(){
 		 String nextLine = System.getProperty("line.separator");
 		 return nextLine;
 	}
-	/**
-	 * 获取images 路径
-	 * @return
-	 */
-	public static String getImages(){
-		return getRootPath() + "images/" ;
-	}
 	
-	/**
-	 * 获取Txt 路径
-	 * @return
-	 */
-	public static String getTxt(){
-		return getRootPath() + "txt" ;
-	}
 	
 }

491e8ab720dc710ff384043ef782676208b1e033
hujunr <hujunr151@163.com>
2019-05-05 11:42:49 +0800

    设计模式系相关添加

---
+++ src/main/java/com/artisan/commonUtils/design_pattern/test000.java
@@ -0,0 +1,71 @@
+/*
+ * www.unisinsight.com Inc.
+ * Copyright (c) 2018 All Rights Reserved
+ */
+package com.artisan.commonUtils.design_pattern;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * description
+ *
+ * @author hujun [KF.hujunA@h3c.com]
+ * @date 2019/04/10 14:15
+ * @since 1.0
+ */
+public class test000 {
+
+    public static void main(String[] args) {
+//        String a = "sadasda,123456";
+//        String[] b = a.split(",");
+//        String c =b[1];
+//        System.out.println(c);
+
+//        String str = "小学,初中,高中,大专,本科,研究生,博士";
+//        String[] buff = str.split(",");
+//        for(int i=0;i<buff.length;i++){
+//            System.out.println(buff[i]);
+//        }
+//        String ids = "1,2,3,4,5,6";
+//        String[] str = ids.split(",");
+//        List<String> idList = Arrays.asList(str);
+//        for (String a : idList) {
+//            System.out.print(a);
+//        }
+
+        List<String> result = Lists.newArrayList();
+        System.out.println( result.size());
+
+
+
+
+
+
+
+
+
+
+
+//        List list = new ArrayList();
+//        list.add("1");
+//        list.add("2");
+//        list.add("3");
+////第一种
+//        String[] arr1 = new String[list.size()];
+//
+//        System.out.println(arr1);
+//        for (int i = 0; i < arr1.length; i++) {
+//            System.out.println(arr1[i]);
+//        }
+////第二种
+//        String[] arr2 = (String[]) list.toArray(new String[0]);
+//        for (int i = 0; i < arr2.length; i++) {
+//            System.out.println(arr2[i]);
+//        }
+    }
+
+}
